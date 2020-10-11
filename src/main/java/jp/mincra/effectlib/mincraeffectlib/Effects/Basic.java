57e895854702f257fc9db24c73198e9e3c8a489f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 13:41:32 +0900

    Delete LocationManager folder and create LocationManager.java

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/Effects/Basic.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/Effects/Basic.java
@@ -6,25 +6,19 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 
-import static org.bukkit.Particle.*;
+
 
 public class Basic {
 
     public Basic(Entity targetentity, String[] args) {
         //usage: /<command> basic <particle> [self/target] <count>
+        LocationManager lm = new LocationManager();
         Location loc = targetentity.getLocation();
-        Vector vec = new Vector(0f,0f,1f);
-        float anglePitch = loc.getPitch();
-        float angleYaw = loc.getYaw();
-        vec.rotateAroundX(Math.toRadians(anglePitch));
-        vec.rotateAroundY(-Math.toRadians(angleYaw));
-        vec.normalize();
-        vec.multiply(5f);
         int count = 10;
         if (args[2].equals("target")) {
             targetentity.sendMessage(ChatColor.GREEN+"[MincraEffectlib]"+ChatColor.RESET+"未実装です");
         } else {
-            loc.add(vec);
+            loc = lm.CasterFront(targetentity);
         }
         if (args[3]!=null) {
             count = Integer.valueOf(args[3]);
