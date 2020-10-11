909d155dd47695a5d0ca386cea1a7ff396ad8871
mankan000 <mankan.zenseki.114514@gmail.com>
2020-10-11 17:58:42 +0900

    straggle with vector

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/Effects/Basic.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/Effects/Basic.java
@@ -14,13 +14,13 @@ public class Basic {
         LocationManager lm = new LocationManager();
         Location loc = targetentity.getLocation();
         int count = 10;
+        if (args[3]!=null) {
+            count = Integer.valueOf(args[3]);
+        }
         if (args[2].equals("target")) {
             targetentity.sendMessage(ChatColor.GREEN+"[MincraEffectlib]"+ChatColor.RESET+"未実装です");
         } else {
             loc = lm.CasterFront(targetentity);
-        }
-        if (args[3]!=null) {
-            count = Integer.valueOf(args[3]);
         }
         Particle particle = Particle.valueOf(args[1]);
         loc.getWorld().spawnParticle(particle,loc,count);
--- src/main/java/jp/mincra/effectlib/mincraeffectlib/LocationManager.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/LocationManager.java
@@ -4,16 +4,40 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
 public class LocationManager {
     public Location CasterFront(Entity caster){
         Location loc = caster.getLocation();
+        loc.add(CasterEyeDirection(loc).multiply(5));
+        return loc;
+    }
+    public Location LookingAt(Entity caster) {
+        Location loc = caster.getLocation();
+        Vector cv = CasterEyeDirection(loc);
+        Collection<Entity> c = caster.getNearbyEntities(10d,10d,10d);
+        Double rad ;
+        List<Vector> el = new ArrayList<>();
+        Vector v = new Vector(0,0,0) ;
+        for(Entity e : c){
+            Location l = e.getLocation();
+            v.setX(l.getX() - loc.getX());
+            v.setY(l.getY() - loc.getY());
+            v.setX(l.getZ() - loc.getZ());
+
+        }
+        return loc;
+    }
+    private Vector CasterEyeDirection(Location loc){
         Vector vec = new Vector(0f,0f,1f);
         float anglePitch = loc.getPitch();
         float angleYaw = loc.getYaw();
         vec.rotateAroundX(Math.toRadians(anglePitch));
         vec.rotateAroundY(-Math.toRadians(angleYaw));
-        vec.multiply(5f);
-        loc.add(vec);
-        return loc;
+        vec.normalize();
+        return vec;
     }
 }
