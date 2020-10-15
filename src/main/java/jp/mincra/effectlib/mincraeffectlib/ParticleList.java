a062a893cd6e9caf6f40c5a287c7f338dd327e90
mankan000 <mankan.zenseki.114514@gmail.com>
2020-10-15 13:43:01 +0900

    static list particlelist

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/ParticleList.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/ParticleList.java
@@ -5,20 +5,18 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class ParticleList {
+    private static final List<String> plist = setParticleList();
     public ParticleList(Entity caster,String[] args){
-        if (args[0] == "random") {
-            String[] plist = {"ASH","BARRIER","BLOCK_CRACK","BLOCK_DUST","BUBBLE_COLUMN_UP","BUBBLE_POP","CAMPFIRE_COSY_SMOKE","CAMPFIRE_SIGNAL_SMOKE","CLOUD","COMPOSTER","CRIMSON_SPORE","CRIT","CRIT_MAGIC","CURRENT_DOWN","DAMAGE_INDICATOR","DOLPHIN","DRAGON_BREATH","DRIP_LAVA","DRIP_WATER","DRIPPING_HONEY","DRIPPING_OBSIDIAN_TEAR","ENCHANTMENT_TABLE","END_ROD","EXPLOSION_HUGE","EXPLOSION_LARGE","EXPLOSION_NORMAL","FALLING_DUST","FALLING_HONEY","FALLING_LAVA","FALLING_NECTAR","FALLING_OBSIDIAN_TEAR","FALLING_WATER","FIREWORKS_SPARK","FLAME","FLASH","HEART","ITEM_CRACK","LANDING_HONEY","LANDING_LAVA","LANDING_OBSIDIAN_TEAR","LAVA","LEGACY_BLOCK_CRACK","LEGACY_BLOCK_DUST","LEGACY_FALLING_DUST","MOB_APPEARANCE","NAUTILUS","NOTE","PORTAL","REDSTONE","REVERSE_PORTAL","SLIME","SMOKE_LARGE","SMOKE_NORMAL","SNEEZE","SNOW_SHOVEL","SNOWBALL","SOUL","SOUL_FIRE_FLAME","SPELL","SPELL_INSTANT","SPELL_MOB","SPELL_MOB_AMBIENT","SPELL_WITCH","SPIT","SQUID_INK","SUSPENDED","SUSPENDED_DEPTH","SWEEP_ATTACK","TOTEM","TOWN_AURA","VILLAGER_ANGRY","VILLAGER_HAPPY","WARPED_SPORE","WATER_BUBBLE","WATER_DROP","WATER_SPLASH","WATER_WAKE","WHITE_ASH"};
+        if (args[0].equals("random")) {
             Random random = new Random();
             int r = random.nextInt(78);
-            caster.sendMessage(plist[r]);
-            Particle particle = Particle.valueOf(plist[r]);
-            LocationManager lm = new LocationManager();
+            caster.sendMessage(plist.get(r));
+            Particle particle = Particle.valueOf(plist.get(r));
             LivingEntity le = (LivingEntity) caster;
-            caster.getLocation().getWorld().spawnParticle(particle,lm.CasterFront(le),1);
+            caster.getLocation().getWorld().spawnParticle(particle,LocationManager.CasterFront(le),1);
         } else {
             caster.sendMessage("ASH  /" +
                     "BARRIER  /" +
@@ -99,5 +97,8 @@ public class ParticleList {
                     "WATER_WAKE  \n" +
                     "WHITE_ASH");
         }
+    }
+    private static List<String> setParticleList(){
+        return new ArrayList<>(Arrays.asList("ASH", "BARRIER", "BLOCK_CRACK", "BLOCK_DUST", "BUBBLE_COLUMN_UP", "BUBBLE_POP", "CAMPFIRE_COSY_SMOKE", "CAMPFIRE_SIGNAL_SMOKE", "CLOUD", "COMPOSTER", "CRIMSON_SPORE", "CRIT", "CRIT_MAGIC", "CURRENT_DOWN", "DAMAGE_INDICATOR", "DOLPHIN", "DRAGON_BREATH", "DRIP_LAVA", "DRIP_WATER", "DRIPPING_HONEY", "DRIPPING_OBSIDIAN_TEAR", "ENCHANTMENT_TABLE", "END_ROD", "EXPLOSION_HUGE", "EXPLOSION_LARGE", "EXPLOSION_NORMAL", "FALLING_DUST", "FALLING_HONEY", "FALLING_LAVA", "FALLING_NECTAR", "FALLING_OBSIDIAN_TEAR", "FALLING_WATER", "FIREWORKS_SPARK", "FLAME", "FLASH", "HEART", "ITEM_CRACK", "LANDING_HONEY", "LANDING_LAVA", "LANDING_OBSIDIAN_TEAR", "LAVA", "LEGACY_BLOCK_CRACK", "LEGACY_BLOCK_DUST", "LEGACY_FALLING_DUST", "MOB_APPEARANCE", "NAUTILUS", "NOTE", "PORTAL", "REDSTONE", "REVERSE_PORTAL", "SLIME", "SMOKE_LARGE", "SMOKE_NORMAL", "SNEEZE", "SNOW_SHOVEL", "SNOWBALL", "SOUL", "SOUL_FIRE_FLAME", "SPELL", "SPELL_INSTANT", "SPELL_MOB", "SPELL_MOB_AMBIENT", "SPELL_WITCH", "SPIT", "SQUID_INK", "SUSPENDED", "SUSPENDED_DEPTH", "SWEEP_ATTACK", "TOTEM", "TOWN_AURA", "VILLAGER_ANGRY", "VILLAGER_HAPPY", "WARPED_SPORE", "WATER_BUBBLE", "WATER_DROP", "WATER_SPLASH", "WATER_WAKE", "WHITE_ASH"));
     }
 }
