1c15167fbe33c93f9dc5e289597b8fe010db7538
mankan000 <mankan.zenseki.114514@gmail.com>
2020-10-22 11:29:13 +0900

    create preclac for calculation for MagicCircle

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/PreCalc.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/PreCalc.java
@@ -10,67 +10,94 @@ public class PreCalc {
         for(float i=0;i<360;i=i+2F){
             posMCHc[1][ii] = Math.sin(Math.toRadians(i));
             posMCHc[2][ii] = Math.cos(Math.toRadians(i));
-            ii += 1;
+            ii ++;
         }
         //1画
-        float y=0.309F;
+        ii = 0;
         for(float x=-0.95F;x<0.95F;x=x+0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = 0.309D;
+            ii ++;
         }
         //2画
-        y=0.309F;
+        double y = 0.309D;
+        ii = 0;
         for(float x=0.95F;x>-0.58F;x=x-0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
-            y=y-0.0360F;
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
+            y=y-0.0360D;
         }
         //3画
-        y=-0.809F;
+        y=-0.809D;
+        ii = 0;
         for(float x=-0.58F;x<0F;x=x+0.01F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
-            y=y+0.03007F;
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
+            y=y+0.03007D;
         }
         //4画
-        y=1F;
+        y=1D;
+        ii = 0;
         for(float x=0F;x<0.58F;x=x+0.01F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y-0.03007F;
         }
         //5画
         y=-0.809F;
+        ii = 0;
         for(float x=0.5877F;x>-0.951F;x=x-0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y+0.0360F;
         }
         //以下から5角形の描画
         //1画
         y=1F;
+        ii = 0;
         for(float x=0F;x<0.95F;x=x+0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y-0.0360F;
         }
         //2画
         y=0.309F;
+        ii = 0;
         for(float x=0.95F;x>0.58F;x=x-0.01F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y-0.03007F;
         }
         //3画
-        y=-0.809F;
+        ii = 0;
         for(float x=0.58F;x>-0.58F;x=x-0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = -0.809F;
+            ii ++;
         }
         //4画
         y=-0.809F;
+        ii = 0;
         for(float x=-0.58F;x>-0.95F;x=x-0.01F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y+0.03007F;
         }
         //5画
         y=0.309F;
+        ii = 0;
         for(float x=-0.95F;x<0F;x=x+0.05F){
-            loc.getWorld().spawnParticle(particle, loc.getX()+x*kakeru, loc.getY()+p, loc.getZ()+y*kakeru, 1, 0, 0, 0, 0);
+            posMCH[1][1][ii] = x;
+            posMCH[1][2][ii] = y;
+            ii ++;
             y=y+0.0360F;
         }
-    }
     }
 }
