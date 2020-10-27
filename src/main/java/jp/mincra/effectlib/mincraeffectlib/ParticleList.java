2a0f90214f2628aee86d167dac263e11bcba2af4
mankan000 <mankan.zenseki.114514@gmail.com>
2020-10-27 23:40:27 +0900

    make safe/unsafe particle list

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/ParticleList.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/ParticleList.java
@@ -12,7 +12,9 @@ import java.util.*;
 
 public class ParticleList {
     private static final List<String> plist = setParticleList();
-    private static final String plist2 = setParticleListForString();
+    private static final String listAsString = setParticleListForString();
+    public static final List<String> SafeList = setSafeParticleList();
+    public static final List<String> UnsafeList = setUnSafeParticleList();
     public ParticleList(CommandSender ccaster, String[] args){
         Random random = new Random();
         int r = random.nextInt(78);
@@ -20,7 +22,7 @@ public class ParticleList {
             Entity caster = (Entity) ccaster;
             if (args.length < 2) {
                 caster.sendMessage(ChatColor.GREEN + "[MincraEffectLib]" + ChatColor.BLUE + "List of Particle");
-                caster.sendMessage(plist2);
+                caster.sendMessage(listAsString);
            } else if (args[1].equals("random")) {
                 caster.sendMessage(plist.get(r));
                 Particle particle = Particle.valueOf(plist.get(r));
@@ -32,7 +34,7 @@ public class ParticleList {
         } else {
             if (args.length < 2){
                 Bukkit.getLogger().info(ChatColor.GREEN + "[MincraEffectLib]" + ChatColor.BLUE + "List of Particle");
-                Bukkit.getLogger().info(String.valueOf(plist2));
+                Bukkit.getLogger().info(String.valueOf(listAsString));
             } else if (args[1].equals("random")){
                 Bukkit.getLogger().info(String.valueOf(plist.get(r)));
             } else {
@@ -44,6 +46,14 @@ public class ParticleList {
         return new ArrayList<>(Arrays.asList("ASH", "BARRIER", "BLOCK_CRACK", "BLOCK_DUST", "BUBBLE_COLUMN_UP", "BUBBLE_POP", "CAMPFIRE_COSY_SMOKE", "CAMPFIRE_SIGNAL_SMOKE", "CLOUD", "COMPOSTER", "CRIMSON_SPORE", "CRIT", "CRIT_MAGIC", "CURRENT_DOWN", "DAMAGE_INDICATOR", "DOLPHIN", "DRAGON_BREATH", "DRIP_LAVA", "DRIP_WATER", "DRIPPING_HONEY", "DRIPPING_OBSIDIAN_TEAR", "ENCHANTMENT_TABLE", "END_ROD", "EXPLOSION_HUGE", "EXPLOSION_LARGE", "EXPLOSION_NORMAL", "FALLING_DUST", "FALLING_HONEY", "FALLING_LAVA", "FALLING_NECTAR", "FALLING_OBSIDIAN_TEAR", "FALLING_WATER", "FIREWORKS_SPARK", "FLAME", "FLASH", "HEART", "ITEM_CRACK", "LANDING_HONEY", "LANDING_LAVA", "LANDING_OBSIDIAN_TEAR", "LAVA", "LEGACY_BLOCK_CRACK", "LEGACY_BLOCK_DUST", "LEGACY_FALLING_DUST", "MOB_APPEARANCE", "NAUTILUS", "NOTE", "PORTAL", "REDSTONE", "REVERSE_PORTAL", "SLIME", "SMOKE_LARGE", "SMOKE_NORMAL", "SNEEZE", "SNOW_SHOVEL", "SNOWBALL", "SOUL", "SOUL_FIRE_FLAME", "SPELL", "SPELL_INSTANT", "SPELL_MOB", "SPELL_MOB_AMBIENT", "SPELL_WITCH", "SPIT", "SQUID_INK", "SUSPENDED", "SUSPENDED_DEPTH", "SWEEP_ATTACK", "TOTEM", "TOWN_AURA", "VILLAGER_ANGRY", "VILLAGER_HAPPY", "WARPED_SPORE", "WATER_BUBBLE", "WATER_DROP", "WATER_SPLASH", "WATER_WAKE", "WHITE_ASH"));
     }
 
+    private static List<String> setSafeParticleList(){
+        //Unsafe Particle list
+        return new ArrayList<>(Arrays.asList("ASH", "BARRIER", "BUBBLE_COLUMN_UP", "BUBBLE_POP", "CAMPFIRE_COSY_SMOKE", "CAMPFIRE_SIGNAL_SMOKE", "CLOUD", "COMPOSTER", "CRIMSON_SPORE", "CRIT", "CRIT_MAGIC", "CURRENT_DOWN", "DAMAGE_INDICATOR", "DOLPHIN", "DRAGON_BREATH", "DRIP_LAVA", "DRIP_WATER", "DRIPPING_HONEY", "DRIPPING_OBSIDIAN_TEAR", "ENCHANTMENT_TABLE", "END_ROD", "EXPLOSION_HUGE", "EXPLOSION_LARGE", "EXPLOSION_NORMAL", "FALLING_HONEY", "FALLING_LAVA", "FALLING_NECTAR", "FALLING_OBSIDIAN_TEAR", "FALLING_WATER", "FIREWORKS_SPARK", "FLAME", "FLASH", "HEART", "ITEM_CRACK", "LANDING_HONEY", "LANDING_LAVA", "LANDING_OBSIDIAN_TEAR", "LAVA", "MOB_APPEARANCE", "NAUTILUS", "NOTE", "PORTAL", "REVERSE_PORTAL", "SLIME", "SMOKE_LARGE", "SMOKE_NORMAL", "SNEEZE", "SNOW_SHOVEL", "SNOWBALL", "SOUL", "SOUL_FIRE_FLAME", "SPELL", "SPELL_INSTANT", "SPELL_MOB", "SPELL_MOB_AMBIENT", "SPELL_WITCH", "SPIT", "SQUID_INK", "SUSPENDED", "SUSPENDED_DEPTH", "SWEEP_ATTACK", "TOTEM", "TOWN_AURA", "VILLAGER_ANGRY", "VILLAGER_HAPPY", "WARPED_SPORE", "WATER_BUBBLE", "WATER_DROP", "WATER_SPLASH", "WATER_WAKE", "WHITE_ASH"));
+    }
+    private static List<String> setUnSafeParticleList(){
+        //Unsafe Particle list
+        return new ArrayList<>(Arrays.asList("BLOCK_CRACK", "BLOCK_DUST", "FALLING_DUST", "ITEM_CRACK", "LEGACY_BLOCK_DUST", "LEGACY_FALLING_DUST", "REDSTONE"));
+    }
     private static String setParticleListForString(){
         StringBuilder ss = new StringBuilder();
         for (String s : plist) {
