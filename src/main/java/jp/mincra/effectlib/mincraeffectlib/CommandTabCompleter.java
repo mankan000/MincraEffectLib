1eff7ec603347be661516115fc8ff8877fd27322
mankan000 <mankan.zenseki.114514@gmail.com>
2020-11-02 23:29:26 +0900

    tab complete

--- src/main/java/jp/mincra/effectlib/mincraeffectlib/CommandTabCompleter.java
+++ src/main/java/jp/mincra/effectlib/mincraeffectlib/CommandTabCompleter.java
@@ -21,15 +21,58 @@ public class CommandTabCompleter implements TabCompleter {
             if (args[0].length() == 0) { // /testまで
                 return Arrays.asList("effectlist", "particlelist","gettype","basic","magiccirclehorizon","magiccirclehorizonanimation");
             } else {
-                //入力されている文字列と先頭一致
                 if ("effectlist".startsWith(args[0])) {
                     return Collections.singletonList("effectlist");
                 } else if ("particlelist".startsWith(args[0])) {
                     return Collections.singletonList("particlelist");
+                } else if ("gettype".startsWith(args[0])) {
+                    return Collections.singletonList("gettype");
+                } else if ("basic".startsWith(args[0])) {
+                    return Collections.singletonList("basic");
+                } else if ("magiccirclehorizon".startsWith(args[0])) {
+                    return Collections.singletonList("magiccirclehorizon");
+                } else if ("magiccirclehorizonanimation".startsWith(args[0])) {
+                    return Collections.singletonList("magiccirclehorizonanimation");
+                }
+            }
+        } else if (args.length == 2) {
+            //effectlistとprticlelistとgettype以外では第二引数の候補にパーティクルリストを表示
+            if (args[1].length() == 0 && !args[0].equalsIgnoreCase("effectlist") && !args[0].equalsIgnoreCase("particlelist") && !args[0].equalsIgnoreCase("gettype")) { // /testまで
+                return ParticleList.plist;
+            } else if (args[1].length() == 0 && args[0].equalsIgnoreCase("particlelist")){
+                return Collections.singletonList("random");
+            } else {
+                for (String s:ParticleList.plist) {
+                    if (s.startsWith(args[1])) {
+                        return Collections.singletonList(s);
+                    }
+                }
+                if ("random".startsWith(args[1])) {
+                    return Collections.singletonList("random");
+                }
+            }
+        } else if (args.length == 3) {
+            //第三引数ではパーティクルの追加情報を。
+            if (args[2].length() == 0) {
+                return ParticleList.plist;
+            } else {
+                //入力されている文字列と先頭一致
+                if ("effectlist".startsWith(args[2])) {
+                    return Collections.singletonList("effectlist");
+                } else if ("particlelist".startsWith(args[2])) {
+                    return Collections.singletonList("particlelist");
+                } else if ("gettype".startsWith(args[2])) {
+                    return Collections.singletonList("gettype");
+                } else if ("basic".startsWith(args[2])) {
+                    return Collections.singletonList("basic");
+                } else if ("magiccirclehorizon".startsWith(args[2])) {
+                    return Collections.singletonList("magiccirclehorizon");
+                } else if ("magiccirclehorizonanimation".startsWith(args[2])) {
+                    return Collections.singletonList("magiccirclehorizonanimation");
                 }
             }
         }
         //JavaPlugin#onTabComplete()を呼び出す
-        return Collections.singletonList("null");
+        return null;
     }
 }
